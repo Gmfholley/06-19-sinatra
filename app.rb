1156771959a4e662f1114487b32d83d3fe0cd404
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-20 16:47:38 -0500

    Changes method names in theatre_manager to prettier urls Updates the calls for app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'pry'
 require 'sinatra/reloader'
 require 'active_support.rb'
 require 'active_support/core_ext/object/blank.rb'
@@ -9,8 +10,46 @@ CONNECTION.results_as_hash = true
 CONNECTION.execute("PRAGMA foreign_keys = ON;")
 
 get "/home" do
-  (1...1000).each do |x|
-    x.to_s
-  end
+  @m = TheatreManager.new
+  @menu = @m.home
+  erb :menu
+end
 
+get "/movie" do
+  m = TheatreManager.new
+  @menu = m.movie
+  @clas = "movie"
+  erb :menu
+end
+
+get "/theatre" do
+  m = TheatreManager.new
+  @menu = m.theatre
+  @class = "location"
+  erb :menu
+end
+
+get "/location_time" do
+  m = TheatreManager.new
+  @menu = m.location_time
+  @class = "location_time"
+  erb :menu
+end
+
+get "/analyze" do
+  m = TheatreManager.new
+  @menu = m.analyze
+  @class = ""
+  erb :menu
+end
+
+get "/create/movie" do
+  m = TheatreManager.new
+  @menu = m.analyze
+  @class = ""
+  erb :menu
+end
+
+get "/:other" do
+  erb :not_appearing
 end

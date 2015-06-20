8e587a5cafe48bb78ed2333a0ef6305e7b2d8395
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-20 13:43:34 -0500

    Refer to deleted files from the original folder Updates app.rb to require them

--- app.rb
+++ app.rb
@@ -2,15 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'active_support.rb'
 require 'active_support/core_ext/object/blank.rb'
-require_relative 'database_connector.rb'
-require_relative 'location.rb'
-require_relative 'rating.rb'
-require_relative 'studio.rb'
-require_relative 'timeslot.rb'
-require_relative 'movie.rb'
-require_relative 'location_time.rb'
-require_relative 'menu.rb'
-require_relative 'menu_item.rb'
+require_relative '/Users/gwendolyn/code/06-12-Database/lib/theatre_manager.rb'
 
 CONNECTION=SQLite3::Database.new("movies.db")
 CONNECTION.results_as_hash = true

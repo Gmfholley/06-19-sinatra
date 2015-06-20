daf79015b64c25f4fe2a7dfdb04e43b78ea82169
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-20 13:39:55 -0500

    Deletes older files

--- menu_item.rb
+++
@@ -1,9 +0,0 @@
-class MenuItem
-  attr_reader :key_user_returns, :user_message, :do_if_chosen
-  
-  def initialize(args)
-    @key_user_returns = args[:key_user_returns].to_s
-    @user_message = args[:user_message]
-    @do_if_chosen = args[:do_if_chosen]
-  end
-end

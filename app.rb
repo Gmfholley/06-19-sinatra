e35c68b5b2685424c63ac9747615ef9bdd3e1569
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-23 22:51:14 -0500

    Small change to app.rb to correct typo

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ get "/location_time" do
   m = TheatreManager.new
   @menu = m.location_time
   @class_name = LocationTime
-  erb :menu
+  erb :menu_without_links
 end
 
 get "/analyze" do
@@ -85,7 +85,7 @@ get "/show/:something" do
   m = TheatreManager.new()
   
   @menu = m.user_choice_of_object_in_class(@class_name)
-  @menu.title = "Here are all the #{params["something"]}."
+  @menu.title = "Here are all the #{params["something"].pluralize}."
   erb :menu_without_links
   
 end
@@ -109,7 +109,7 @@ get "/delete/:something/:x" do
     @message = "Successfully deleted."
     erb :message
   else
-    @message = "This object was not found or was in another table.  Not deleted."
+    @message = "This #{params["something"]} was not found or was in another table.  Not deleted."
     erb :message
   end
   
@@ -165,21 +165,20 @@ end
 
 
 get "/get_available_locations" do
-  
   m = TheatreManager.new
   @menu = m.available
-  params["something"] = "get_sold_time_locations"
+  params["something"] = "get_available_locations"
   erb :menu
 end
 
-get "/get_available_locations/:something" do
+get "/get_available_locations/:something" do  
   if params["something"] == "available"
     @m = Location.where_available(true)
   else
     @m = Location.where_available(false)
   end
   
-  @menu = Menu.new("The #{params["something"]} times and locations are:")
+  @menu = Menu.new("The #{params["something"].humanize.downcase} times and locations are:")
   @m.each do |lt|
     @menu.add_menu_item(user_message: lt.to_s)
   end
@@ -218,19 +217,44 @@ get "/get_movies_like_this" do
   erb :menu
 end
 
-get "/get_movies_like_this/:x" do
-  if params["x"] == "sold_out"
-    @m = LocationTime.where_sold_out(true)
-  else
-    @m = LocationTime.where_sold_out(false)
-  end
+get "/get_movies_like_this/:something" do
+  @class_name = slash_to_class_names[params["something"]]
+  m = TheatreManager.new()  
+  @menu = m.user_choice_of_object_in_class(@class_name)
+  @menu.title = "Which #{@class_name} do you want to look up?"
+  erb :menu
+end
+
+get "/get_movies_like_this/:something/:x" do
+  all_movies = Movie.where_match("#{params["something"]}_id", params["x"].to_i, "==")
   
-  @menu = Menu.new("The #{params["something"]} times and locations are:")
-  @m.each do |lt|
-    @menu.add_menu_item(user_message: lt.to_s)
+  @menu = Menu.new("These are the movies that match this #{params["something"]}.")
+  all_movies.each do |movie|
+    @menu.add_menu_item(user_message: movie.to_s)
   end
   
   erb :menu_without_links
+
+end
+
+get "/get_num_staff_needed" do
+  @class_name = TimeSlot
+  m = TheatreManager.new()  
+  @menu = m.user_choice_of_object_in_class(@class_name)
+  @menu.title = "Which #{@class_name} do you want to look up the number of staff for?"
+  params["something"] = "get_num_staff_needed"
+  erb :menu
+
+end
+
+get "/get_num_staff_needed/:x" do
+  binding.pry
+  params["something"] = "analyze"
+  m = TimeSlot.create_from_database(params["x"].to_i)
+  binding.pry
+  @message = "You will need #{m.num_staff_needed} staff members for the #{m.name} time slot."
+  erb :message
+
 end
 
 
@@ -240,5 +264,5 @@ end
 
 ###################
 def slash_to_class_names
-  {"theatre" => Location, "location_time" => LocationTime, "movie" => Movie, "time" => TimeSlot}
+  {"theatre" => Location, "location_time" => LocationTime, "movie" => Movie, "time" => TimeSlot, "studio" => Studio, "rating" => Rating}
 end

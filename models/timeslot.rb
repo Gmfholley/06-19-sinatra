473c3975f0cf0d667c0d6ee8ebf39ed552cf9257
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-16 20:52:29 -0500

    Adds ActiveRecord timeslot model

---
+++ models/timeslot.rb
@@ -0,0 +1,17 @@
+class TimeSlot < ActiveRecord::Base
+  validates :time_slot, presence: true
+  
+  # returns the total staff needed for a particular time slot
+  #
+  # returns an Integer
+  def num_staff_needed
+    sum = 0
+    query_string = "SELECT SUM(locations.num_staff) FROM locationtimes INNER JOIN timeslots ON locationtimes.timeslot_id = timeslots.id INNER JOIN locations ON locationtimes.location_id = locations.id WHERE timeslots.id = #{@id} GROUP BY locationtimes.location_id;"
+    staff_array = CONNECTION.execute(query_string)
+    staff_array.each do |hash|
+      sum += hash["SUM(locations.num_staff)"]
+    end
+    sum
+  end
+  
+end

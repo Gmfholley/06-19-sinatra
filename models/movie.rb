064891bba5460e09838124294061ce35c1e4e9a3
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-16 22:43:11 -0500

    Updates model to include special > 0 validation length

--- models/movie.rb
+++ models/movie.rb
@@ -2,15 +2,29 @@ class Movie < ActiveRecord::Base
   belongs_to :rating
   belongs_to :studio
   
+  has_and_belongs_to_many :locations, through: :location_times
+  
+  
   validates :name, presence: true
   validates :description, presence: true
+  validate :length_greater_than_zero
   
-  # TODO - look up how to say length must be an integer greater than 0
-  validates :length, presence: true, numericality: {integer: true, greater_than_zero: true}
+
+  validates :length, presence: true, numericality: {only_integer: true}
   
   validates :studio, presence: true
   validates :rating, presence: true
-
+  
+  
+  # if the number of seats is zero or below, it adds an error
+  #
+  # returns errors Array
+  def length_greater_than_zero
+    if length <= 0
+      errors.add(:length, "must be greater than zero.")
+    end
+    errors
+  end
 
   
 end

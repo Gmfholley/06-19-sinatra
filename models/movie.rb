d2dd9af24d8a07754fe5aa63b2300faae37ef31f
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-19 10:42:58 -0500

    Replaces has_and_belongs_to_many rel with has_many

--- models/movie.rb
+++ models/movie.rb
@@ -2,8 +2,9 @@ class Movie < ActiveRecord::Base
   belongs_to :rating
   belongs_to :studio
   
-  has_and_belongs_to_many :locations, through: :location_times
-  has_and_belongs_to_many :time_slots, through: :location_times  
+  has_many :location_times
+  has_many :locations, through: :location_times
+  has_many :time_slots, through: :location_times
   
   validates :name, presence: true
   validates :description, presence: true

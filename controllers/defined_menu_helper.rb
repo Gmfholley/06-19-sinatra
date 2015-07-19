c538a618d665be0d445476df1f5aa2de978513f5
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-19 10:24:39 -0500

    Adds location to Location hash conversion

--- controllers/defined_menu_helper.rb
+++ controllers/defined_menu_helper.rb
@@ -1,5 +1,5 @@
 module MenuHelper
   def slash_to_class_names
-    {"theatre" => Location, "location_time" => LocationTime, "movie" => Movie, "time" => TimeSlot, "studio" => Studio, "rating" => Rating}
+    {"theatre" => Location, "location_time" => LocationTime, "movie" => Movie, "time" => TimeSlot, "studio" => Studio, "rating" => Rating, "location" => Location}
   end
 end
